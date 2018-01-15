92773ebb9e40bec662a84adc51c90e0955e065de
2015pgcaca44 <35236887+2015pgcaca44@users.noreply.github.com>
2018-01-16 02:47:46 +0530

    Add files via upload

---
+++ privacy.php
@@ -0,0 +1,109 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Find Your Perfect Partner-Matrimonial Site for lovers
+ | Privacy :: Matrimonial
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+<?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="grid_3">
+  <div class="container">
+   <div class="breadcrumb1">
+     <ul>
+        <a href="index.php"><i class="fa fa-home home_1"></i></a>
+        <span class="divider">&nbsp;|&nbsp;</span>
+        <li class="current-page">Privacy Policy</li>
+     </ul>
+   </div>
+   <div class="terms_1">
+	    <h3>The standard chunk of Lorem Ipsum used since the 1500s is reproduced</h3>	
+	    <ul class="feature_list">
+			<li><a href="#">1. There are many variations of passages of Lorem Ipsum available.</a></li>
+			<li><a href="#">2. It is a long established fact that a reader.</a></li>
+			<li><a href="#">3. Nulla rutrum nisi mi, iaculis commodo nibh lobortis sed.</a></li>
+			<li><a href="#">4. Sed pulvinar, nunc vitae molestie dapibus, lacus dolor dignissim sapien.</a></li>
+			<li><a href="#">5. Pellentesque ipsum ex, imperdiet quis consequat sed, consectetur ut ante.</a></li>
+			<li><a href="#">6. Aliquam libero felis, mollis vitae elementum vel, bibendum eu tortor.</a></li>
+			<li><a href="#">7. Lorem ipsum dolor sit amet.</a></li>
+		</ul>
+	    <h5>It is a long established fact that a reader will be distracted by the readable content of a page </h5>   
+	    <ul class="feature_list feature_list1">
+			<li><a href="#">There are many variations of passages of Lorem Ipsum available.</a></li>
+			<li><a href="#">It is a long established fact that a reader.</a></li>
+			<li><a href="#">Nulla rutrum nisi mi, iaculis commodo nibh lobortis sed.</a></li>
+			<li><a href="#">Sed pulvinar, nunc vitae molestie dapibus, lacus dolor dignissim sapien.</a></li>
+			<li><a href="#">Pellentesque ipsum ex, imperdiet quis consequat sed, consectetur ut ante.</a></li>
+			<li><a href="#">Aliquam libero felis, mollis vitae elementum vel, bibendum eu tortor.</a></li>
+			<li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+		</ul>
+		<div>
+		<p>I spend 23 years of my life to studying and gaining informaition through books and my classmates.
+Now,I want to spend my next few month in the shadow of someone who teaches me something
+ beyond the knowladge</p>
+<p>gained as a university graduate and provides me with an in hand experience of how the relationship 
+between team members is maintained and how 
+they worked togather to achieve a commen goal.</p>
+<p>I'm sharing an interasting story with you,when i got my first pc desktop in the year 2018,and that sound
+ windows XP,that was the day I found my real interest.
+From that day I was never able to reject someone either it is to repair windows or searching for a fault in pc. 
+Whatever drives me, I found happiness in this industry and I love to work with someone with whom 
+I'm emotionally attached</p>
+
+<p>I would only say that I'm excited your company and it's prospects.please give me a chance to prove myself.
+</p>
+Thank you.
+<p>
+
+I have good knowladge of web-development, I Love to work on the designing of web pages. I have a good
+ skill in HTML,CSS,JavaScript,PHP even i did  few projects using these.
+I can say that my knowladge ,skills and my attitude of learning are beneficial for your company. One thing which
+ make me different  from other
+is hard work using that key </p>I can achieve anything and it keeps me updated  in the development field.
+<p>
+As a web-developer I can say that I am perfect for internship,rest decision depends on you. I would only say 
+that I am excited about your company 
+and its prospects. please give me a chance to prove myself.    </p> 
+     </div>
+   </div>
+</div>
+
+
+<?php include_once("footer.php");?>
+
+</body>
+</html>	
